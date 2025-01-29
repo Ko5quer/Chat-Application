90df5a3defa3c71da477a2beccc74ba4936c49bd
Prince <juniortlou212@gmail.com>
2025-01-29 12:56:37 +0200

    deleted test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-try:
-    with open("chat.txt", "r") as chat:
-        chat.write("This is a test message\n")
-    print("File created and written successfully.")
-except Exception as e:
-    print(f"Error: {e}")
