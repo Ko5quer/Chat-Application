dc07576ea861f699cb953455a8e690012eab6265
Prince <juniortlou212@gmail.com>
2025-01-30 19:43:06 +0200

    Fixed bugs and improved ui

--- Main.py
+++ Main.py
@@ -1,5 +1,5 @@
 
-from message import write_message,check
+from message import write_message,check, change
 import time 
 
 
@@ -10,23 +10,26 @@ name=str(input("Enter your username: "))
 while True:
     try:
         with open("chat.txt","r") as file:
-            choice=int(input("1.Write message \n2.Refresh \n3.Exit \nPlease choose what you want to do: "))
+            print("*****Chat-Application-System*****")
+            try:
+                choice=int(input("1.Write message \n2.Refresh \n3.Exit \nPlease choose what you want to do: "))
 
-            if (choice==1): 
-                        write_message(name)
-                        #to delay it and stop spam messages
-                        time.sleep(10)
-                        time.sleep(10)
-            elif (choice==2):
-                if (check()):
-                    write_message(name)
+                if (choice==1): 
+                            write_message(name)
+                            #to delay it and stop spam messages
+                            time.sleep(10)
+                elif (choice==2):
+                    if (check()):
+                        print ("New message detected")
+                    else:
+                        print("No new messages found, try again")
+                elif (choice==3):
+                    print("Exiting the system. Goodbye!")
+                    break
                 else:
-                     print("No new messages found, try again")
-            elif (choice==3):
-                print("Exiting the system. Goodbye!")
-                break
-            else:
-                print("Please choose between 1,2 and 3")      
+                    print("Please choose between 1,2 and 3")
+            except ValueError:
+                 print("Please choose between 1,2 and 3")      
 
     except FileNotFoundError:
         #To create a file when a file doesnt exist
